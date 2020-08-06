39a6fd2f11f100225e19493d94a03d488f8f834c
windasrk <windasrk@gmail.com>
2020-08-06 10:53:12 +0700

    tugas blade selesai

---
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -0,0 +1,44 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/" class="brand-link">
+      <img src=" {{ asset('/adminlte/dist/img/AdminLTELogo.png')}} "
+           alt="AdminLTE Logo"
+           class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light" href="/">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview">
+            <a href="/" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/data-tables" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Tables
+              </p>
+            </a>
+          </li>
+      
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
